e1b5b6d19758be5f621b98ea3b3864b01b979006
Ankit Mishra <100961335+AnkitMishra47@users.noreply.github.com>
2022-10-05 10:24:19 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class RWOnly {
+
+    private int     A   = 10 ; 
+    private String  B   = "Hello";
+    
+    public void setA(int A)
+    {
+        this.A = a ; 
+    }
+    
+     public void setA(String B)
+     {
+        this.B = b ; 
+     }
+    
+     public int getA()
+     {
+        return A;  
+     }
+     public String getB()
+     {
+        return B ; 
+     }
     
 }
